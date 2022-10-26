fe0e6c748fdf57d062b434804d9d5ef9f652a20b
Dmytro Vaskovets <dmitriy.vaskovets@gmail.com>
2022-10-26 11:57:36 +0300

    Adding first lesson homework

---
+++ lesson1/homework.py
@@ -0,0 +1,38 @@
+
+#     Задача: Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+first = 10
+second = 30
+result = first + second
+print(result)
+result = first - second
+print(result)
+result = first * second
+print(result)
+result = first / second
+print(result)
+result = first ** second
+print(result)
+result = first // second
+print(result)
+result = first % second
+print(result)
+
+#     Задача: Створіть змінну і почергово запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+comparison = first > second
+print(comparison)
+comparison = first < second
+print(comparison)
+comparison = first == second
+print(comparison)
+comparison = first <= second
+print(comparison)
+comparison = first >= second
+print(comparison)
+comparison = first != second
+print(comparison)
+
+#     Задача: Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world". Виведіть на екран.
+str1 = 'Hello '
+str2 = 'world'
+concatenation = str1 + str2
+print(concatenation)
