a291aa27f762ba323536659b786baf96c5c93dc4
Dmytro Vaskovets <dmitriy.vaskovets@gmail.com>
2022-12-13 22:50:56 +0200

    Uploading 12th homework.

---
+++ lesson12/homework 12.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+import math
+
+
+class Point:
+    x = None
+    y = None
+
+    def __init__(self, x, y):
+        if isinstance(x, float | int):
+            self.x = x
+        if isinstance(y, float | int):
+            self.y = y
+
+
+class Figure(ABC):
+    pass
+    # @abstractmethod
+    # def area(self):
+    #     pass
+    #
+    # @abstractmethod
+    # def length(self):
+    #     pass
+
+
+class Line(Figure):
+    begin = None
+    end = None
+
+    def __init__(self, begin, end):
+        # check if Point-class obj received
+        if isinstance(begin, Point):
+            self.begin = begin
+        if isinstance(end, Point):
+            self.end = end
+
+    def length(self):
+        result = ((self.begin.x - self.end.x) ** 2 + (self.begin.y - self.end.y) ** 2) ** 0.5
+        return result
+    # area = length
+
+
+class Triangle(Figure):
+    def __init__(self, a, b, c):
+        if isinstance(a, Point):
+            self.a = a
+        if isinstance(b, Point):
+            self.b = b
+        if isinstance(c, Point):
+            self.c = c
+
+    def area(self):
+        ab_length = math.dist([self.a.x, self.a.y], [self.b.x, self.b.y])
+        bc_length = math.dist([self.b.x, self.b.y], [self.c.x, self.c.y])
+        ac_length = math.dist([self.a.x, self.a.y], [self.c.x, self.c.y])
+        half_perimetr = (ab_length + bc_length + ac_length) / 2
+        area = math.sqrt(half_perimetr * (half_perimetr - ab_length) * (half_perimetr - bc_length) * (half_perimetr - ac_length))
+        return area
+
+
+p1 = Point(0,  0)
+p2 = Point(3,  4)
+p3 = Point(10, 2)
+
+my_triangle = Triangle(p1, p2, p3)
+
+print(my_triangle.area())
